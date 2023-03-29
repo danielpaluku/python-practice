ee968f5334e79d1ac91bf0fa966c8e05048fb2a8
Daniel Paluku <danielpaluku@gmail.com>
2023-03-29 16:43:42 -0400

    I have commented a line of code to practice

--- number_of_ways.py
+++ number_of_ways.py
@@ -22,7 +22,7 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
     for i in tqdm(range(k)):
         for path in paths:
             new_path = path.copy()
-            last_position = new_path[-1]
+            last_position = new_path[-1] #Comment for the purpose of practice
 
             # exist fast if not going to make to end
             if endPos - last_position > (k - i - 1):
